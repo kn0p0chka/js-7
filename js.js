34056e8e5d3d1631194b64080531af00d017c984
Sophiya Evsykova <s.evsykova@gmail.com>
2022-03-08 12:56:02 +0100

    me

---
+++ js.js
@@ -0,0 +1,48 @@
+
+function mas() {
+    let me = {
+        name: 'sonya',
+        age: '13',
+        school: 'Optima',
+        
+        adress: {
+            str: 'sinkevicha',
+            building: '7',
+            floor: '2',
+            flat: '8',
+            country:'PL'
+        }
+    }
+    console.log(me)
+    
+}
+
+mas()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
